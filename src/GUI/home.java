a58aeb12ea891bc458db7fca6092692c391bcc96
Nguyen Hieu <nguyenhieu@Nguyens-MacBook-Air.local>
2021-10-30 10:11:55 +0700

    Create code's folder

---
+++ src/GUI/home.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package GUI;
+
+/**
+ * @author nguyenhieu
+ *
+ */
+public class home {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
